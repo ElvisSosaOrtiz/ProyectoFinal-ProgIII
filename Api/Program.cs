c8b6641d2d8fdcc78bcd5fa8e4d7988cf72b6010
elvis <elvissosaortiz27@gmail.com>
2024-12-05 17:07:30 -0400

    feat: Add dependency injection for the created interfaces

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,3 +1,8 @@
+using Repositories;
+using RepositoryContracts;
+using ServiceContracts;
+using Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Dependency injection
+builder.Services
+    .AddScoped<IAsignaturaRepository, AsignaturaRepository>()
+    .AddScoped<IEstudianteAsignaturaRepository, EstudianteAsignaturaRepository>()
+    .AddScoped<IEstudianteRepository, EstudianteRepository>()
+    .AddScoped<IEstudianteService, EstudianteService>();
 
 var app = builder.Build();
 
