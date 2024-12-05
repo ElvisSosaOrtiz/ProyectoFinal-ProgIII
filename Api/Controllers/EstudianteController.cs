050cae1e89fe63ee271b37f92df2d94fc77c4d9d
elvis <elvissosaortiz27@gmail.com>
2024-12-05 17:07:57 -0400

    feat: Implement endpoints

---
+++ Api/Controllers/EstudianteController.cs
@@ -0,0 +1,26 @@
+﻿namespace Api.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using ServiceContracts;
+    using Shared.Request;
+    using Shared.Routing;
+
+    [ApiController]
+    [Route(EstudianteControllerRoutes.Root)]
+    public class EstudianteController(IEstudianteService service) : ControllerBase
+    {
+        [HttpGet(EstudianteControllerRoutes.Student + "/{id}")]
+        public IActionResult GetStudentById(int id)
+        {
+            var result = service.GetStudentById(id);
+            return Ok(result);
+        }
+
+        [HttpPost(EstudianteControllerRoutes.Student)]
+        public IActionResult RegisterStudent([FromBody] RequestOfRegisterStudent request)
+        {
+            service.RegisterStudent(request);
+            return Ok();
+        }
+    }
+}
---
+++ Shared/Routing/EstudianteControllerRoutes.cs
@@ -0,0 +1,8 @@
+﻿namespace Shared.Routing
+{
+    public class EstudianteControllerRoutes
+    {
+        public const string Root = "api/estudiante";
+        public const string Student = "student";
+    }
+}
