cd87744bda694878fab16e3956b02e0df2c121cc
20198880 <20198880@itla.edu.do>
2024-12-06 02:55:10 -0400

    chore: adicion de metodo buscar estudiante por id

--- Repositories/EstudianteRepository.cs
+++ Repositories/EstudianteRepository.cs
@@ -1,13 +1,15 @@
 ﻿namespace Repositories
 {
     using Entities;
+    using Microsoft.EntityFrameworkCore;
     using RepositoryContracts;
 
     public class EstudianteRepository(AppDbContext.UniversidadDbContext context) : IEstudianteRepository
     {
         public Estudiante? GetStudentById(int userId)
         {
-            return context.Estudiantes.Find(userId);
+            return context.Estudiantes.Include(t => t.Titulacion).
+                FirstOrDefault(e => e.EstudianteId == userId);
         }
 
         public void AddAndSaveStudent(Estudiante Estudiante)
