09d0aed409a01df2b13ff39d6fbb4d46becd8310
elvis <elvissosaortiz27@gmail.com>
2024-12-06 17:07:28 -0400

    feat: Dependency injection

--- Api/Program.cs
+++ Api/Program.cs
@@ -15,17 +15,20 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 // Dependency injection
-//builder.Services
-//.AddScoped<IAsignaturaRepository, AsignaturaRepository>()
-//.AddScoped<IEstudianteAsignaturaRepository, EstudianteAsignaturaRepository>()
+builder.Services
+    .AddScoped<IAsignaturaRepository, AsignaturaRepository>()
+    .AddScoped<IEstudianteAsignaturaRepository, EstudianteAsignaturaRepository>()
+    .AddScoped<IEstudianteRepository, EstudianteRepository>()
+    .AddScoped<IProfesorRepository, ProfesorRepository>()
+    .AddScoped<IEstudianteService, EstudianteService>()
+    .AddScoped<IAsignaturaService, AsignaturaService>()
+    .AddScoped<IEstudianteAsignaturaService, EstudianteAsignaturaService>()
+    .AddScoped<IProfesorService, ProfesorService>();
 
 builder.Services.AddDbContext<UniversidadDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-
-builder.Services.AddScoped<IEstudianteRepository, EstudianteRepository>();
-builder.Services.AddScoped<IEstudianteService, EstudianteService>();
 
 var app = builder.Build();
 
